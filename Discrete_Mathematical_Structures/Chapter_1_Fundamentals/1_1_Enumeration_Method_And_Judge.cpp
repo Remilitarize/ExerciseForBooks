e14f0e5fb3ed461a9a0d8c66caed3010484ab571
Remilitarize <2473108024@qq.com>
2018-06-18 10:50:39 +0800

    不属于符号

---
+++ Discrete_Mathematical_Structures/Chapter_1_Fundamentals/1_1_Enumeration_Method_And_Judge.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<set>
+#include<string>
+#include<sstream>
+using namespace std;
+
+set<string> s;
+set<string>::iterator it;
+
+bool find(const set<string> s, const string element){
+	for(it = s.begin(); it != s.end(); it ++){
+		if(element.compare(*it) == 0){
+			return true;
+		}
+	}
+	return false;
+}
+
+int main(){
+
+    cout << "请输入集合 A 中的元素，以空格分隔（不包含集合元素）：" << endl;
+
+    string temp;
+    getline(cin, temp);
+    stringstream ss(temp);
+
+    while(ss >> temp){
+        s.insert(temp);
+    }
+
+    cout << "\n输入的集合 A 为：" << endl;
+    for (it = s.begin(); it != s.end(); it ++){
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    do{
+        cout << "\n请输入要查找的元素：" << endl;
+        cin >> temp;
+        if(find(s, temp)){
+        	cout << temp << " 属于 A" << endl;
+		}
+		else{
+			cout << temp << " 不属于 A" << endl;
+		}
+    }while(1);
+
+    return 0;
+}
